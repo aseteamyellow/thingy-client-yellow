e009d5e6d1f54abafda0f2eb833325021c475a53
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-10-23 17:18:48 +0200

    Launch activity

---
+++ app/src/main/java/ch/snipy/thingyClientYellow/RegisterLoginActivity.kt
@@ -0,0 +1,13 @@
+package ch.snipy.thingyClientYellow
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class RegisterLoginActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_login)
+    }
+
+}
