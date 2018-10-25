30c485bfadfe42e4c2bbc1f1a3691ad1133d9a04
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-10-25 16:00:26 +0200

    implements the signal methods

--- app/src/main/java/ch/snipy/thingyClientYellow/RegisterLoginActivity.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/RegisterLoginActivity.kt
@@ -3,11 +3,30 @@ package ch.snipy.thingyClientYellow
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 
-class RegisterLoginActivity : FragmentActivity() {
+class RegisterLoginActivity : FragmentActivity(), LoginSignal, RegisterSignal {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_login)
+    }
+
+    override fun onRegisterSignal() {
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        val registerFragment = RegisterActivityFragment.newInstance()
+        registerFragment.loginSignal = this
+        transaction.replace(R.id.container, registerFragment, null)
+        transaction.commit()
+    }
+
+    override fun onLoginSignal() {
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        val loginFragment = LoginActivityFragment.newInstance()
+        loginFragment.registerSignal = this
+        transaction.replace(R.id.container, loginFragment, null)
+        transaction.commit()
     }
 
 }
