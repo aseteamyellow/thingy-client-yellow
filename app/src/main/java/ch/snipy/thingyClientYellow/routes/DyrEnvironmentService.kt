8a273a6563f3a5ddbc831dd546fb7385932e6256
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-10-28 21:22:11 +0100

    define routes for the environment management

---
+++ app/src/main/java/ch/snipy/thingyClientYellow/routes/DyrEnvironmentService.kt
@@ -0,0 +1,35 @@
+package ch.snipy.thingyClientYellow.routes
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.http.*
+
+interface DyrEnvironmentService {
+    @GET("/{userId}")
+    fun getEnvironments(@Path("userId") userId: String): Call<Any>
+
+    @GET("/{envId}")
+    fun getEnvironment(@Path("envId") envId: String): Call<Any>
+
+    @POST("/")
+    fun createEnvironment(): Call<Any>
+
+    @PATCH("/{envId}")
+    fun updateEnvironment(@Path("envId") envId: String): Call<Any>
+
+    @DELETE("/{envId}")
+    fun deleteEnvironment(@Path("envId") envId: String): Call<Any>
+
+    @PUT("/enableNotif/{envId}")
+    fun enableNotification(@Path("envId") envId: String): Call<Any>
+
+    companion object Factory {
+        fun create(): DyrEnvironmentService {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(null) // TODO
+                .baseUrl("/environment")
+                .build()
+            return retrofit.create(DyrEnvironmentService::class.java)
+        }
+    }
+}
