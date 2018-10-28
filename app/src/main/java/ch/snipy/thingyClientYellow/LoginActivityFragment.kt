6099a8d8d056b07a50a86eb9bafa23cfcc7f2614
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-10-28 11:32:36 +0100

    Remove clickListener and add the onClick action directly to the xml

--- app/src/main/java/ch/snipy/thingyClientYellow/LoginActivityFragment.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/LoginActivityFragment.kt
@@ -4,24 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class LoginActivityFragment : Fragment() {
 
-    lateinit var registerSignal: RegisterSignal
-
     companion object {
         fun newInstance(): LoginActivityFragment {
-            return LoginActivityFragment()
+            val fragment = LoginActivityFragment()
+            return fragment
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
         val view = inflater.inflate(R.layout.fragment_login, container, false)
-        val signUpButton = view.findViewById<Button>(R.id.sign_in_nav)
-        signUpButton.setOnClickListener { _ -> registerSignal.registerSignal() }
+        super.onCreateView(inflater, container, savedInstanceState)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/ch/snipy/thingyClientYellow/RegisterActivityFragment.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/RegisterActivityFragment.kt
@@ -4,27 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class RegisterActivityFragment : Fragment() {
 
-    lateinit var loginSignal: LoginSignal
-
     companion object {
         fun newInstance(): RegisterActivityFragment {
-            return RegisterActivityFragment()
+            val fragment = RegisterActivityFragment()
+            return fragment
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_register, container, false)
-        val loginButton = view.findViewById<Button>(R.id.sign_in_nav)
-        loginButton.setOnClickListener { _ -> loginSignal.loginSignal() }
         return view
     }
 }
