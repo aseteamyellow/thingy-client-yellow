d5a9845014ab6b4f993624664ef6da5d077a7b47
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-10-29 12:50:30 +0100

    change types for the routes

--- app/src/main/java/ch/snipy/thingyClientYellow/routes/DyrEnvironmentService.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/routes/DyrEnvironmentService.kt
@@ -1,5 +1,7 @@
 package ch.snipy.thingyClientYellow.routes
 
+import android.os.Environment
+import ch.snipy.thingyClientYellow.Model
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.http.*
@@ -7,22 +9,22 @@ import retrofit2.http.*
 interface DyrEnvironmentService {
     // TODO modidy the routes because they are the same...
     @GET("/{userId}")
-    fun getEnvironments(@Path("userId") userId: String): Call<Any>
+    fun getEnvironments(@Path("userId") userId: Int): Call<List<Model.Environment>>
 
     @GET("/{envId}")
-    fun getEnvironment(@Path("envId") envId: String): Call<Any>
+    fun getEnvironment(@Path("envId") envId: Int): Call<Model.Environment>
 
     @POST("/")
-    fun createEnvironment(): Call<Any>
+    fun createEnvironment(): Call<Environment>
 
     @PATCH("/{envId}")
-    fun updateEnvironment(@Path("envId") envId: String): Call<Any>
+    fun updateEnvironment(@Path("envId") envId: Int): Call<Environment>
 
     @DELETE("/{envId}")
-    fun deleteEnvironment(@Path("envId") envId: String): Call<Any>
+    fun deleteEnvironment(@Path("envId") envId: Int): Call<Any>
 
     @PUT("/enableNotif/{envId}")
-    fun enableNotification(@Path("envId") envId: String): Call<Any>
+    fun enableNotification(@Path("envId") envId: Int): Call<Any>
 
     companion object Factory {
         fun create(): DyrEnvironmentService {
