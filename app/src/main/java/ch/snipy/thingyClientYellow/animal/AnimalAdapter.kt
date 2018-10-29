1bc8ed923ae03ec0d17b72fed3b93d9895100016
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-10-29 14:52:24 +0100

    refactoring

--- app/src/main/java/ch/snipy/thingyClientYellow/animal/AnimalAdapter.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/animal/AnimalAdapter.kt
@@ -3,8 +3,9 @@ package ch.snipy.thingyClientYellow.animal
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import ch.snipy.thingyClientYellow.Animal
 
-class AnimalAdapter(private val dataset: Array<String>) : RecyclerView.Adapter<AnimalAdapter.ViewHolder>() {
+class AnimalAdapter(private val dataset: Array<Animal>) : RecyclerView.Adapter<AnimalAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalAdapter.ViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
--- app/src/main/java/ch/snipy/thingyClientYellow/animal/AnimalsFragment.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/animal/AnimalsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import ch.snipy.thingyClientYellow.Animal
 import ch.snipy.thingyClientYellow.R
 
 class AnimalsFragment : Fragment() {
@@ -26,7 +27,12 @@ class AnimalsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.animals_fragment, container, false)
         viewManager = LinearLayoutManager(activity)
-        viewAdapter = AnimalAdapter(dataset = arrayOf("Cat", "Dog")) // TODO call the API
+        viewAdapter = AnimalAdapter(
+            dataset = arrayOf(
+                Animal(null, "Cat"),
+                Animal(null, "Dog")
+            )
+        ) // TODO call the API
 
         recyclerView = rootView.findViewById<RecyclerView>(R.id.animals_recycler_view).apply {
             setHasFixedSize(true)
