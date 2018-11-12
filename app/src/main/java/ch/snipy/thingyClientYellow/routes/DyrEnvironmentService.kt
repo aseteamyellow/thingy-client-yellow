1b47e9d8bc367eb962d2f9a78a8ace29dd58fa42
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-12 15:31:14 +0100

    Change return type to Observable and add the adapter factory call

--- app/src/main/java/ch/snipy/thingyClientYellow/routes/DyrEnvironmentService.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/routes/DyrEnvironmentService.kt
@@ -1,34 +1,37 @@
 package ch.snipy.thingyClientYellow.routes
 
 import android.os.Environment
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
 
 interface DyrEnvironmentService {
 
     @GET("/all/{userId}")
-    fun getEnvironments(@Path("userId") userId: Int): Call<List<Environment>>
+    fun getEnvironments(@Path("userId") userId: Int): Observable<List<Environment>>
 
     @GET("/one/{envId}")
-    fun getEnvironment(@Path("envId") envId: Int): Call<Environment>
+    fun getEnvironment(@Path("envId") envId: Int): Observable<Environment>
 
     @POST("/{userId}")
-    fun createEnvironment(@Path("userId") userId: Int): Call<Environment>
+    fun createEnvironment(@Path("userId") userId: Int): Observable<Environment>
 
     @PATCH("/{envId}")
-    fun updateEnvironment(@Path("envId") envId: Int): Call<Environment>
+    fun updateEnvironment(@Path("envId") envId: Int): Observable<Environment>
 
     @DELETE("/{envId}")
-    fun deleteEnvironment(@Path("envId") envId: Int): Call<String>
+    fun deleteEnvironment(@Path("envId") envId: Int): Observable<String>
 
     @PUT("/enableNotif/{envId}")
-    fun enableNotification(@Path("envId") envId: Int, @Body body: Map<String, String>): Call<String>
+    fun enableNotification(@Path("envId") envId: Int, @Body body: Map<String, String>): Observable<String>
 
     companion object Factory : DyrServiceFactory {
         fun create(): DyrEnvironmentService {
             val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl("$baseUrl/environment/")
                 .build()
