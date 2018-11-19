386a6f826701d6ee8d83ae4ef0b2bdf24730ad6b
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-19 13:07:29 +0100

    bind the environment to a view holder

--- app/src/main/java/ch/snipy/thingyClientYellow/environment/EnvironmentAdapter.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/environment/EnvironmentAdapter.kt
@@ -1,15 +1,15 @@
 package ch.snipy.thingyClientYellow.environment
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ch.snipy.thingyClientYellow.Environment
 import ch.snipy.thingyClientYellow.R
-import ch.snipy.thingyClientYellow.environment.EnvironmentAdapter.ViewHolder
+import ch.snipy.thingyClientYellow.environment.EnvironmentAdapter.EnvironmentItemViewHolder
 import kotlinx.android.synthetic.main.environment_list_item.view.*
 
 class EnvironmentAdapter(
@@ -17,10 +17,10 @@ class EnvironmentAdapter(
     val context: Context,
     private val listener: RecyclerViewListener
 ) :
-    RecyclerView.Adapter<ViewHolder>() {
+    RecyclerView.Adapter<EnvironmentItemViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EnvironmentItemViewHolder {
+        return EnvironmentItemViewHolder(
             LayoutInflater.from(context).inflate(
                 R.layout.environment_list_item,
                 parent,
@@ -34,15 +34,22 @@ class EnvironmentAdapter(
         return dataset.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: EnvironmentItemViewHolder, position: Int) {
         holder.nameTextView.text = dataset[position].name
+        holder.image.setImageResource(R.mipmap.viva_logo)
+        holder.environment = dataset[position]
     }
 
-    class ViewHolder(val view: View, val listener: RecyclerViewListener) : RecyclerView.ViewHolder(view),
+    class EnvironmentItemViewHolder(
+        val view: View,
+        private val listener: RecyclerViewListener
+    ) :
+        RecyclerView.ViewHolder(view),
         View.OnClickListener {
 
+        lateinit var environment: Environment
         val nameTextView: TextView = view.environment_item_name
-        val image = view.environment_item_logo
+        val image: ImageView = view.environment_item_logo
 
         init {
             view.setOnClickListener(this)
