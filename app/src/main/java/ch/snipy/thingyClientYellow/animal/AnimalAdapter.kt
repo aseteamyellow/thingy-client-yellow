5db1578dc5642b113c83f53b5bdc11d448c728ca
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-19 15:06:53 +0100

    implement animal adapter for the recyclerview

--- app/src/main/java/ch/snipy/thingyClientYellow/animal/AnimalAdapter.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/animal/AnimalAdapter.kt
@@ -1,6 +1,7 @@
 package ch.snipy.thingyClientYellow.animal
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -8,23 +9,31 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ch.snipy.thingyClientYellow.Animal
 import ch.snipy.thingyClientYellow.AnimalsItemViewListener
+import ch.snipy.thingyClientYellow.R
 import kotlinx.android.synthetic.main.animal_list_item.view.*
 
 class AnimalAdapter(
     private val dataset: Array<Animal>,
-    val context: Context,
+    private val context: Context,
     private val listener: AnimalsItemViewListener
 ) :
     RecyclerView.Adapter<AnimalAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalAdapter.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalAdapter.ViewHolder = ViewHolder(
+        LayoutInflater.from(context).inflate(
+            R.layout.animal_list_item,
+            parent,
+            false
+        ),
+        listener
+    )
 
     override fun getItemCount(): Int = dataset.size
 
     override fun onBindViewHolder(holder: AnimalAdapter.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.nameTextView.text = dataset[position].name
+        holder.image.setImageResource(R.mipmap.default_animal_logo)
+        holder.animal = dataset[position]
     }
 
     class ViewHolder(
