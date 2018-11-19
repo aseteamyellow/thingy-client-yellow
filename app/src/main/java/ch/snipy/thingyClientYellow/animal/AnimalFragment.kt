2b66540dccb154c40f7c5ff6649298c18d9c21f5
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-19 16:08:59 +0100

    view one animal base sample

---
+++ app/src/main/java/ch/snipy/thingyClientYellow/animal/AnimalFragment.kt
@@ -0,0 +1,45 @@
+package ch.snipy.thingyClientYellow.animal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import ch.snipy.thingyClientYellow.Animal
+import ch.snipy.thingyClientYellow.R
+
+class AnimalFragment : Fragment() {
+
+    // Data
+    lateinit var animal: Animal
+
+    // UI
+    private lateinit var photo: ImageView
+    private lateinit var name: TextView
+
+    // For API call TODO
+    // val environmentService by lazy { DyrEnvironmentService.create() }
+    // var disposable: Disposable? = null
+
+    companion object {
+        fun newInstance(animal: Animal): AnimalFragment {
+            val fragment = AnimalFragment()
+            fragment.animal = animal
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val rootView = inflater.inflate(R.layout.fragment_environment_animals, container, false)
+
+        photo = rootView.findViewById(R.id.animal_photo)
+        name = rootView.findViewById(R.id.animal_name)
+
+        name.text = animal.name
+
+        return rootView
+    }
+}
