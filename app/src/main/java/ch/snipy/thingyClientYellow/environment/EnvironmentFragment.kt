feac2b9450b179fab4ae7a0198ae3f520d3e8005
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-19 14:40:36 +0100

    change Id for name and type and bind them at creation

--- app/src/main/java/ch/snipy/thingyClientYellow/environment/EnvironmentFragment.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/environment/EnvironmentFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import ch.snipy.thingyClientYellow.Environment
 import ch.snipy.thingyClientYellow.R
@@ -12,10 +13,17 @@ class EnvironmentFragment : Fragment() {
 
     private lateinit var environment: Environment
 
+    // UI field
+
+    private lateinit var name: TextView
+    private lateinit var type: TextView
+
     companion object {
         fun newInstance(environment: Environment): EnvironmentFragment {
             val fragment = EnvironmentFragment()
             fragment.environment = environment
+            // TODO call service
+
             return fragment
         }
     }
@@ -23,6 +31,13 @@ class EnvironmentFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_environment, container, false)
         super.onCreateView(inflater, container, savedInstanceState)
+
+        name = view.findViewById(R.id.fragment_environment_name)
+        type = view.findViewById(R.id.fragment_environment_type)
+
+        name.text = environment.name
+        type.text = environment.envType
+
         return view
     }
 }
