5e0466b4e078fe6e1bc754f9f184f33ea0672372
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-19 15:39:27 +0100

    setup the recyclerview for the animals from one environment

--- app/src/main/java/ch/snipy/thingyClientYellow/environment/EnvironmentFragment.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/environment/EnvironmentFragment.kt
@@ -6,15 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ch.snipy.thingyClientYellow.Animal
+import ch.snipy.thingyClientYellow.AnimalsItemViewListener
 import ch.snipy.thingyClientYellow.Environment
 import ch.snipy.thingyClientYellow.R
+import ch.snipy.thingyClientYellow.animal.AnimalAdapter
 
 class EnvironmentFragment : Fragment() {
 
+    // For the recycler view
+    private lateinit var recyclerView: RecyclerView
+
+    // Listener for animal's items
+    private lateinit var listener: AnimalsItemViewListener
+
+    // The current environment
     private lateinit var environment: Environment
 
     // UI field
-
     private lateinit var name: TextView
     private lateinit var type: TextView
 
@@ -29,15 +40,30 @@ class EnvironmentFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_environment, container, false)
         super.onCreateView(inflater, container, savedInstanceState)
 
-        name = view.findViewById(R.id.fragment_environment_name)
-        type = view.findViewById(R.id.fragment_environment_type)
+        val rootView = inflater.inflate(R.layout.fragment_environment, container, false)
+
+        recyclerView = rootView.findViewById<RecyclerView>(R.id.animals_recycler_view).apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(activity)
+            adapter = AnimalAdapter(
+                dataset = listOf(
+                    Animal(null, "Cat"),
+                    Animal(null, "Dog"),
+                    Animal(null, "Bird")
+                ),
+                context = context!!,
+                listener = listener
+            )
+        }
+
+        name = rootView.findViewById(R.id.fragment_environment_name)
+        type = rootView.findViewById(R.id.fragment_environment_type)
 
         name.text = environment.name
         type.text = environment.envType
 
-        return view
+        return rootView
     }
 }
