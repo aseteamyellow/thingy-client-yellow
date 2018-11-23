e16c6a9a1274b005505a7cf003e91ca288e96ae3
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-23 20:00:30 +0100

    add a Todo...

--- app/src/main/java/ch/snipy/thingyClientYellow/Model.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/Model.kt
@@ -11,14 +11,35 @@ data class User(
     @SerializedName("password") val password: String
 )
 
+/* TODO
+{
+    "id": 1,
+    "user_id": 4,
+    "name": "example",
+    "env_type": "aquarium",
+    "humidity": null,
+    "temperature": null,
+    "air_quality": null,
+    "air_pressure": null,
+    "light": null,
+    "humidity_notif": 0,
+    "temperature_notif": 0,
+    "air_quality_notif": 0,
+    "air_pressure_notif": 0,
+    "light_notif": 0,
+    "thingy": "",
+    "animals": []
+}
+ */
+
 data class Environment(
     @SerializedName("id") val id: Int?,
     @SerializedName("name") val name: String,
     @SerializedName("env_type") val envType: String,
     @SerializedName("animals") val animals: List<Animal>,
-    @SerializedName("humidity") val humidity: Double,
-    @SerializedName("temperature") val temperature: Double,
-    @SerializedName("luminosity") val luminosity: Double,
+    @SerializedName("humidity") val humidity: Double?,
+    @SerializedName("temperature") val temperature: Double?,
+    @SerializedName("luminosity") val luminosity: Double?,
     @SerializedName("humidity_notif") val humidityNotification: Boolean,
     @SerializedName("temperature_notif") val temperatureNotification: Boolean,
     @SerializedName("luminosity_notif") val luminosityNotification: Boolean
