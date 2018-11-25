7d97a40975f0340207fa5a75b61f9db4f1eff2a8
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-25 20:04:14 +0100

    bind the button to create an animal

--- app/src/main/java/ch/snipy/thingyClientYellow/environment/EnvironmentFragment.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/environment/EnvironmentFragment.kt
@@ -5,14 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import ch.snipy.thingyClientYellow.Animal
-import ch.snipy.thingyClientYellow.AnimalsItemViewListener
-import ch.snipy.thingyClientYellow.Environment
-import ch.snipy.thingyClientYellow.R
+import ch.snipy.thingyClientYellow.*
 import ch.snipy.thingyClientYellow.animal.AnimalAdapter
 import ch.snipy.thingyClientYellow.routes.DyrAnimalService
 import ch.snipy.thingyClientYellow.routes.DyrEnvironmentService
@@ -34,6 +32,9 @@ class EnvironmentFragment : Fragment() {
     // UI field
     private lateinit var name: TextView
     private lateinit var type: TextView
+
+    // button
+    private lateinit var addAnimalButton: Button
 
     // Api call
     private val environmentService by lazy { DyrEnvironmentService.create() }
@@ -92,11 +93,19 @@ class EnvironmentFragment : Fragment() {
         }
 
         name = rootView.findViewById(R.id.fragment_environment_name)
-        type = rootView.findViewById(R.id.fragment_environment_type)
-
         name.text = environment.name
+
+        type = rootView.findViewById(R.id.fragment_environment_type)
         type.text = environment.envType
 
+        addAnimalButton = rootView.findViewById(R.id.environment_add_animal_button)
+        addAnimalButton.setOnClickListener(::onClickCreateAnimal)
+
         return rootView
+    }
+
+    private fun onClickCreateAnimal(view: View) {
+        Log.d(loggingTag, "create animal button callback, id : ${view.id}")
+        (activity as MainActivity).onClickCreateAnimalNavigation(environment)
     }
 }
