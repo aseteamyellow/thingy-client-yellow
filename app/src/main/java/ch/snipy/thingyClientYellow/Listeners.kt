b9fe1a9328aad100727888069cc26f60f98dfb6b
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-25 21:24:30 +0100

    add callback for dataset change

--- app/src/main/java/ch/snipy/thingyClientYellow/Listeners.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/Listeners.kt
@@ -1,13 +1,24 @@
 package ch.snipy.thingyClientYellow
 
 import android.view.View
+import okhttp3.ResponseBody
 
 interface EnvironmentsItemViewListener {
     fun onEnvironmentItemClick(view: View, environment: Environment)
-    fun onEnvironmentItemDeleteClick(view: View, environment: Environment)
+    fun onEnvironmentItemDeleteClick(
+        view: View,
+        environment: Environment,
+        onSuccess: (ResponseBody) -> Unit,
+        onError: (Throwable) -> Unit
+    )
 }
 
 interface AnimalsItemViewListener {
     fun onAnimalItemClick(view: View, animal: Animal)
-    fun onAnimalItemDeleteClick(view: View, animal: Animal)
+    fun onAnimalItemDeleteClick(
+        view: View,
+        animal: Animal,
+        onSuccess: (ResponseBody) -> Unit,
+        onError: (Throwable) -> Unit
+    )
 }
