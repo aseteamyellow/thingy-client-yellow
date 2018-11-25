2c4d5108b80b3ba48311a56144ffbd34eab7f03d
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-25 22:38:52 +0100

    add navigation to update a fragment

--- app/src/main/java/ch/snipy/thingyClientYellow/animal/AnimalFragment.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/animal/AnimalFragment.kt
@@ -1,16 +1,22 @@
 package ch.snipy.thingyClientYellow.animal
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import ch.snipy.thingyClientYellow.Animal
+import ch.snipy.thingyClientYellow.MainActivity
 import ch.snipy.thingyClientYellow.R
 
 class AnimalFragment : Fragment() {
+
+    // For logging
+    private val loggingTag = "ANIMAL_FRAGMENT"
 
     // Data
     lateinit var animal: Animal
@@ -18,6 +24,9 @@ class AnimalFragment : Fragment() {
     // UI
     private lateinit var photo: ImageView
     private lateinit var name: TextView
+
+    // Button
+    private lateinit var updateButton: ImageButton
 
     // For API call TODO
     // val environmentService by lazy { DyrEnvironmentService.create() }
@@ -37,9 +46,20 @@ class AnimalFragment : Fragment() {
 
         photo = rootView.findViewById(R.id.animal_photo)
         name = rootView.findViewById(R.id.animal_name)
+        updateButton = rootView.findViewById(R.id.animal_fragment_update_button)
+
+        updateButton.setOnClickListener(::onClickUpdateAnimal)
 
         name.text = animal.name
 
         return rootView
+    }
+
+    private fun onClickUpdateAnimal(view: View) {
+        Log.d(loggingTag, "On click update animal from view : ${view.id}")
+        (activity as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.main_activity_frame_layout, AnimalUpdateFragment.newInstance(animal))
+            .addToBackStack(null)
+            .commit()
     }
 }
