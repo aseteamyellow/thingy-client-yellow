c3500ce515a9d147491288aef679f433f4b75859
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-25 16:30:10 +0100

    add a logging

--- app/src/main/java/ch/snipy/thingyClientYellow/routes/DyrServiceFactory.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/routes/DyrServiceFactory.kt
@@ -1,15 +1,21 @@
 package ch.snipy.thingyClientYellow.routes
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
 
 interface DyrServiceFactory {
     val baseUrl: String
         get() = "http://134.21.124.156:8080"
     val client: OkHttpClient
-        get() = OkHttpClient().newBuilder()
-            .connectTimeout(3, TimeUnit.SECONDS)
-            .readTimeout(3, TimeUnit.SECONDS)
-            .writeTimeout(3, TimeUnit.SECONDS)
-            .build()
+        get() {
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+            return OkHttpClient().newBuilder()
+                .addInterceptor(logging)
+                .connectTimeout(3, TimeUnit.SECONDS)
+                .readTimeout(3, TimeUnit.SECONDS)
+                .writeTimeout(3, TimeUnit.SECONDS)
+                .build()
+        }
 }
