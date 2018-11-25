161e9fea239d13cb3cb627e4997d14a9f25e8c89
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-25 16:01:57 +0100

    add object utils for the thingy and the raspberry pi 3

--- app/src/main/java/ch/snipy/thingyClientYellow/Utils.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/Utils.kt
@@ -7,9 +7,28 @@ data class Tuple4<out A, out B, out C, out D>(val _1: A, val _2: B, val _3: C, v
 data class Tuple5<out A, out B, out C, out D, out E>(val _1: A, val _2: B, val _3: C, val _4: D, val _5: E)
 
 object Tuple {
-    operator fun <A> invoke(_1: A): Tuple1<A> = Tuple1(_1)
-    operator fun <A, B> invoke(_1: A, _2: B): Tuple2<A, B> = Tuple2(_1, _2)
-    operator fun <A, B, C> invoke(_1: A, _2: B, _3: C): Tuple3<A, B, C> = Tuple3(_1, _2, _3)
-    operator fun <A, B, C, D> invoke(_1: A, _2: B, _3: C, _4: D): Tuple4<A, B, C, D> = Tuple4(_1, _2, _3, _4)
-    operator fun <A, B, C, D, E> invoke(_1: A, _2: B, _3: C, _4: D, _5: E): Tuple5<A, B, C, D, E> = Tuple5(_1, _2, _3, _4, _5)
+    operator fun <A> invoke(_1: A): Tuple1<A> =
+        Tuple1(_1)
+
+    operator fun <A, B> invoke(_1: A, _2: B): Tuple2<A, B> =
+        Tuple2(_1, _2)
+
+    operator fun <A, B, C> invoke(_1: A, _2: B, _3: C): Tuple3<A, B, C> =
+        Tuple3(_1, _2, _3)
+
+    operator fun <A, B, C, D> invoke(_1: A, _2: B, _3: C, _4: D): Tuple4<A, B, C, D> =
+        Tuple4(_1, _2, _3, _4)
+
+    operator fun <A, B, C, D, E> invoke(_1: A, _2: B, _3: C, _4: D, _5: E): Tuple5<A, B, C, D, E> =
+        Tuple5(_1, _2, _3, _4, _5)
+}
+
+// Thingy
+object Thingy {
+    val name: String = "Yellow"
+}
+
+// Raspeberry pi 3 IP
+object Raspeberry {
+    val ip: IP = Tuple(172, 22, 22, 1)
 }
