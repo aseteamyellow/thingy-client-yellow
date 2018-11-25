1638b556e55275ea9a4459bb8a6f343a2a4feabf
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-11-25 20:50:53 +0100

    add methods for item deletion

--- app/src/main/java/ch/snipy/thingyClientYellow/Listeners.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/Listeners.kt
@@ -4,8 +4,10 @@ import android.view.View
 
 interface EnvironmentsItemViewListener {
     fun onEnvironmentItemClick(view: View, environment: Environment)
+    fun onEnvironmentItemDeleteClick(view: View, environment: Environment)
 }
 
 interface AnimalsItemViewListener {
     fun onAnimalItemClick(view: View, animal: Animal)
+    fun onAnimalItemDeleteClick(view: View, animal: Animal)
 }
