fd2b30bae1a8ff56b4f5829f5988e392ea64e9ea
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-12-09 15:24:13 +0100

    service to obtain the animal's types

---
+++ app/src/main/java/ch/snipy/thingyClientYellow/routes/DyrAnimalTypeService.kt
@@ -0,0 +1,26 @@
+package ch.snipy.thingyClientYellow.routes
+
+import ch.snipy.thingyClientYellow.AnimalType
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface DyrAnimalTypeService {
+
+    @GET("animalType")
+    fun getAnimalTypes(): Observable<List<AnimalType>>
+
+    companion object Factory : DyrServiceFactory {
+        fun create(ipAddress: String = defaultIpAddress): DyrAnimalTypeService {
+            val retrofit = Retrofit.Builder()
+                .client(client)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("http://$ipAddress/environment/")
+                .build()
+            return retrofit.create(DyrAnimalTypeService::class.java)
+        }
+    }
+}
