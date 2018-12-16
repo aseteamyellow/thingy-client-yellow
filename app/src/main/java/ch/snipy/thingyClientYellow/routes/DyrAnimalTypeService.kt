5ea990549ff0e515512123c0af113b8cd53d7a11
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-12-16 19:03:53 +0100

    add authentication token in the header for the animal type

--- app/src/main/java/ch/snipy/thingyClientYellow/routes/DyrAnimalTypeService.kt
+++ app/src/main/java/ch/snipy/thingyClientYellow/routes/DyrAnimalTypeService.kt
@@ -1,16 +1,18 @@
 package ch.snipy.thingyClientYellow.routes
 
 import ch.snipy.thingyClientYellow.AnimalType
+import ch.snipy.thingyClientYellow.Token
 import io.reactivex.Observable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface DyrAnimalTypeService {
 
     @GET("animalType")
-    fun getAnimalTypes(): Observable<List<AnimalType>>
+    fun getAnimalTypes(@Header("token") token: Token): Observable<List<AnimalType>>
 
     companion object Factory : DyrServiceFactory {
         fun create(ipAddress: String = defaultIpAddress): DyrAnimalTypeService {
